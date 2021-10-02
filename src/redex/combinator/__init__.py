f1c165f1f7d861d5d1d2bb28d8288f6bc6eb1d4a
Andrei Nesterov <ae.nesterov@gmail.com>
2021-10-02 23:34:08 +0300

    Add addition combinator

--- src/redex/combinator/__init__.py
+++ src/redex/combinator/__init__.py
@@ -22,30 +22,33 @@ parameters must be definite (e.g. tuple parameters must be specified
 and variadic tuples must not be used)*.
 """
 
-from redex.combinator._base import Combinator
-from redex.combinator._serial import serial, Serial
-from redex.combinator._parallel import parallel, Parallel
-from redex.combinator._select import select, Select
+from redex.combinator._add import add, Add
 from redex.combinator._branch import branch
-from redex.combinator._residual import residual
-from redex.combinator._dup import dup, Dup
+from redex.combinator._base import Combinator
 from redex.combinator._drop import drop, Drop
+from redex.combinator._dup import dup, Dup
 from redex.combinator._identity import identity, Identity
+from redex.combinator._parallel import parallel, Parallel
+from redex.combinator._residual import residual
+from redex.combinator._select import select, Select
+from redex.combinator._serial import serial, Serial
 
 __all__ = [
-    "Combinator",
-    "serial",
-    "Serial",
-    "parallel",
-    "Parallel",
-    "select",
-    "Select",
+    "add",
+    "Add",
     "branch",
-    "residual",
-    "dup",
-    "Dup",
+    "Combinator",
     "drop",
     "Drop",
+    "dup",
+    "Dup",
     "identity",
     "Identity",
+    "parallel",
+    "Parallel",
+    "residual",
+    "select",
+    "Select",
+    "serial",
+    "Serial",
 ]
